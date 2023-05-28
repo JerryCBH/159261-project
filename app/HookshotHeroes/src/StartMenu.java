6bb62eeef73c77404777b3051dfc6039c2196e58
Jerry Hsiung <hsiung_jerry@yahoo.co.nz>
2023-05-28 22:41:39 +1200

    Add start theme.

--- app/HookshotHeroes/src/StartMenu.java
+++ app/HookshotHeroes/src/StartMenu.java
@@ -1,9 +1,11 @@
 import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
 import javax.swing.*;
 import javax.swing.text.DefaultCaret;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
@@ -18,10 +20,13 @@ public class StartMenu{
     private JButton quitButton;
     private Image mainImage;
     private Image gameTitle;
+    private final AudioClip startTheme;
 
     public StartMenu() {
         mainImage = loadImage("MainImage.png");
         gameTitle = loadImage("GameTitle.png");
+        startTheme = loadAudio("start.wav");
+        startAudioLoop(startTheme, 0);
     }
 
     public void show() {
@@ -51,6 +56,8 @@ public class StartMenu{
         easyModeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                stopAudioLoop(startTheme);
+
                 // Start single player mode game
                 var options = new GameOptions();
                 options.SinglePlayerMode = true;
@@ -64,6 +71,8 @@ public class StartMenu{
         normalModeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                stopAudioLoop(startTheme);
+
                 // Start the 2 player mode game
                 // Start single player mode game
                 var options = new GameOptions();
@@ -79,7 +88,9 @@ public class StartMenu{
         missionModeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Start single player mission game
+                stopAudioLoop(startTheme);
+
+                // Start quest game.
                 var options = new GameOptions();
                 options.SinglePlayerMode = true;
                 options.DoublePlayerMode = false;
@@ -148,6 +159,148 @@ public class StartMenu{
 
         // Return null
         return null;
+    }
+
+    public StartMenu.AudioClip loadAudio(String filename) {
+        try {
+            // Open File
+            var stream = getClass().getResourceAsStream(filename);
+
+            if (stream != null) {
+                // Open Audio Input Stream
+                AudioInputStream audio = AudioSystem.getAudioInputStream(new BufferedInputStream(stream));
+
+                // Return Audio Clip
+                return new StartMenu.AudioClip(audio);
+            }
+        } catch (Exception e) {
+            // Catch Exception
+            System.out.println("Error: cannot open Audio File " + filename + "\n" + e.getMessage());
+        }
+
+        // Return Null
+        return null;
+    }
+
+    public void startAudioLoop(StartMenu.AudioClip audioClip, float volume) {
+        // Check audioClip for null
+        if(audioClip == null) {
+            // Print error message
+            System.out.println("Error: audioClip is null\n");
+
+            // Return
+            return;
+        }
+
+        // Get Loop Clip
+        Clip clip = audioClip.getLoopClip();
+
+        // Create Loop Clip if necessary
+        if(clip == null) {
+            try {
+                // Create a Clip
+                clip = AudioSystem.getClip();
+
+                // Load data
+                clip.open(audioClip.getAudioFormat(), audioClip.getData(), 0, (int)audioClip.getBufferSize());
+
+                // Create Controls
+                FloatControl control = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
+
+                // Set Volume
+                control.setValue(volume);
+
+                // Set Clip to Loop
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+                // Set Loop Clip
+                audioClip.setLoopClip(clip);
+            } catch(Exception exception) {
+                // Display Error Message
+                System.out.println("Error: could not play Audio Clip\n");
+            }
+        }
+
+        // Set Frame Position to 0
+        clip.setFramePosition(0);
+
+        // Start Audio Clip playing
+        clip.start();
+    }
+
+    public void stopAudioLoop(StartMenu.AudioClip audioClip) {
+        // Get Loop Clip
+        Clip clip = audioClip.getLoopClip();
+
+        // Check clip is not null
+        if(clip != null){
+            // Stop Clip playing
+            clip.stop();
+        }
+    }
+
+    public class AudioClip {
+        // Format
+        AudioFormat mFormat;
+
+        // Audio Data
+        byte[] mData;
+
+        // Buffer Length
+        long mLength;
+
+        // Loop Clip
+        Clip mLoopClip;
+
+        public Clip getLoopClip() {
+            // return mLoopClip
+            return mLoopClip;
+        }
+
+        public void setLoopClip(Clip clip) {
+            // Set mLoopClip to clip
+            mLoopClip = clip;
+        }
+
+        public AudioFormat getAudioFormat() {
+            // Return mFormat
+            return mFormat;
+        }
+
+        public byte[] getData() {
+            // Return mData
+            return mData;
+        }
+
+        public long getBufferSize() {
+            // Return mLength
+            return mLength;
+        }
+
+        public AudioClip(AudioInputStream stream) {
+            // Get Format
+            mFormat = stream.getFormat();
+
+            // Get length (in Frames)
+            mLength = stream.getFrameLength() * mFormat.getFrameSize();
+
+            // Allocate Buffer Data
+            mData = new byte[(int)mLength];
+
+            try {
+                // Read data
+                stream.read(mData);
+            } catch(Exception exception) {
+                // Print Error
+                System.out.println("Error reading Audio File\n");
+
+                // Exit
+                System.exit(1);
+            }
+
+            // Set LoopClip to null
+            mLoopClip = null;
+        }
     }
 
     public static void main(String[] args) {
